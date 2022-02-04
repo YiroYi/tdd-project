1ffd55a8fd4f630fcfa110aa7418b5424ac4e85e
YiroYi <yiroyi@live.com>
2022-02-03 23:31:06 -0600

    Complete chapter 3

--- js/test_money.js
+++ js/test_money.js
@@ -15,6 +15,24 @@ class Money {
   }
 }
 
+class Portfolio {
+  constructor() {
+    this.moneys = []
+  }
+
+  add(...moneys) {
+    this.moneys = this.moneys.concat(moneys);
+  }
+
+  evaluate(currency) {
+    let total = this.moneys.reduce((sum, money) => {
+      return sum + money.amount
+    }, 0)
+
+    return new Money(total, currency)
+  }
+}
+
 let fiver = new Money(5, "USD")
 let tenner = fiver.times(2)
 assert.strictEqual(tenner.amount, 10)
@@ -30,3 +48,12 @@ let originalMoney = new Money(4002, "KWR")
 let actualMoneyAfterDivision = originalMoney.divide(4)
 let expectedMoneyAfterDivision = new Money(1000.5, "KWR")
 assert.deepStrictEqual(actualMoneyAfterDivision, expectedMoneyAfterDivision)
+
+// New Portfolio test
+
+let fiveDollar = new Money(5, "USD")
+let tenDollar = new Money(10, "USD")
+let fifteenDollars = new Money(15, 'USD')
+let portfolio = new Portfolio();
+portfolio.add(fiveDollar, tenDollar)
+assert.deepStrictEqual(portfolio.evaluate("USD"), fifteenDollars);
